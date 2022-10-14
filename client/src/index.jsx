bee409f6ccb747b9d936d5e0714ada5e9c6025a3
Randy Ip <randyip@gmail.com>
2022-10-14 01:57:42 -0700

    double checking all tokens are gone

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './Main/App.jsx'
-// require('dotenv').config()
 
 ReactDOM.render(<App />, document.getElementById('app'))
