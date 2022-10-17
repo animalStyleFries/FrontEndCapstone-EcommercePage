5fe881a3fe8d02e9c155f12b94e8aaf4b10fb80e
Randy Ip <randyip@gmail.com>
2022-10-16 19:14:09 -0700

    completed add to cart functionality

--- client/src/Main/WidgetRandy/Components/AddtoCart.jsx
+++ client/src/Main/WidgetRandy/Components/AddtoCart.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { useState } from 'react'
+import axios from 'axios'
+import API from '../../../API.js'
 
 const AddtoCart = ({ styleArray, style }) => {
+
+  // ----------------------- Set Up -----------------------
   // set a size state (might need to change to not state)
   const [index, setIndex] = useState(0)
+  const [quantity, setQuantity] = useState(1)
 
   // create an array that contains related information
   let cartInfo = Object.values(styleArray[style].skus) || [];
@@ -17,20 +22,36 @@ const AddtoCart = ({ styleArray, style }) => {
   // create a quantity array to map over
   const quantityArray = [...Array(cartInfo[index].quantity).keys()].map(x => x + 1) || []
 
+  // ----------------------- Functions -----------------------
+
+  //onchange function for quantity
+  const quantityOnChange = (e) => {
+    setQuantity(e.target.value)
+  }
+
   // onchange function for size
   const sizeOnChange = (e) => {
-    console.log('this is e.target.entryIndex', e.target.value)
     setIndex(e.target.value)
-    // const sku_id = cartInfo[index].sku_id
-    // const quantityArray = [...Array(cartInfo[index].quantity).keys()].map(x => x + 1)
+    setQuantity(1)
   }
+
+  // add to cart button
+  const addToCart = (e) => {
+    e.preventDefault()
+    for (let i = 0; i < quantity; i++) {
+      axios.post(API.server + 'cart', { sku_id: sku_id }, { headers: { 'Authorization': API.gitToken } })
+        .catch(err => console.log(err))
+    }
+    console.log('this is quantity and id', quantity, sku_id)
+  }
+
+  // ----------------------- Return Div -----------------------
 
   // set up an indexCounter for entries
   let indexCounter = 0;
-
   return (<div>{
     styleArray.length > 0 && <form>
-      <select>
+      <select value={quantity} onChange={quantityOnChange}>
         {quantityArray.map(entry => (
           <option value={entry}>Qty: {entry}</option>
         ))}
@@ -40,6 +61,7 @@ const AddtoCart = ({ styleArray, style }) => {
           <option value={indexCounter++}>Size: {entry.size}</option>
         ))}
       </select>
+      <button onClick={addToCart}>Add to Cart</button>
     </form>
   }</div>)
 }
