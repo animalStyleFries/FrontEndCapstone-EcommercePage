e940576394a5a4a397d0fee8b3c1e52ea7c8cb33
Randy Ip <randyip@gmail.com>
2022-10-17 01:13:06 -0700

    Fixed Image gallery and style selector

--- client/src/Main/WidgetRandy/Components/ImageGallery.jsx
+++ client/src/Main/WidgetRandy/Components/ImageGallery.jsx
@@ -1,6 +1,8 @@
+// imports
 import React from 'react'
 import { useState } from 'react'
 import SmallerImage from './SmallerComponents/SmallerImage.jsx'
+import styled from 'styled-components'
 
 const ImageGallery = ({ styleArray, style }) => {
 
@@ -14,13 +16,27 @@ const ImageGallery = ({ styleArray, style }) => {
   return (<div>
 
     {/* main picture */}
-    <img src={styleArray[style].photos[currentImage].url}></img>
+    <MainDisplay src={styleArray[style].photos[currentImage].url}></MainDisplay>
 
     {/* map over remainder of entries to smaller images */}
-    {styleArray[style].photos.map(entry => (
-      <SmallerImage entry={entry} index={count++} setCurrentImage={setCurrentImage} key={count} />
-    ))}
+    <DumbNails>
+      {styleArray[style].photos.map(entry => (
+        <SmallerImage entry={entry} index={count++} setCurrentImage={setCurrentImage} key={count} />
+      ))}
+    </DumbNails>
   </div>)
 }
 
+const DumbNails = styled.div`
+  display: grid;
+  width: 50rem;
+  grid-gap: 1.5rem;
+  grid-template-columns: 7rem 7rem 7rem 7rem 7rem 7rem;
+`;
+
+const MainDisplay = styled.img`
+height: 30rem;
+width: 50rem;
+object-fit: cover;
+`
 export default ImageGallery
