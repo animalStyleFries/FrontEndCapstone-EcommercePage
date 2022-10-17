d38df0a4fbc55a326437b49210afb82badde8768
Monica Liu <liuqianmonica@gmail.com>
2022-10-17 14:34:40 -0700

    fix useEffect

--- tests/Fundamentals.test.js
+++ tests/Fundamentals.test.js
@@ -1,21 +1,15 @@
 import renderer from 'react-test-renderer';
 import React from 'react'
-import App from '../client/src/Main/App.jsx';
+import App from '../client/client/src/Main/App.jsx';
 import StyleSelectorCard from "../client/src/Main/WidgetRandy/Components/SmallerComponents/StyleSelectorCard.jsx";
 // const axios = require('axios');
 // jest.mock('axios')
 // axios.get.mockResolvedValue([])
 
-it('Should be able to render', () => {
-  const component = renderer.create(<StyleSelectorCard entry={{ name: 'hi' }} />)
+it('Should render a title and 3 widgets', () => {
+  const component = renderer.create(
+    <App />,
+  );
   let tree = component.toJSON()
-  console.log(tree)
+  expect(tree.children.length).toBe(4)
 })
-
-// it('Should render a title and 3 widgets', () => {
-//   const component = renderer.create(
-//     <App />,
-//   );
-//   let tree = component.toJSON()
-//   expect(tree.children.length).toBe(4)
-// })
