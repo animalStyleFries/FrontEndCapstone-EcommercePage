2732f2a6bd7951e2c475cfd1255d497c032f7494
Randy Ip <randyip@gmail.com>
2022-10-18 14:31:40 -0700

    saving

--- client/src/Main/WidgetRandy/Components/SmallerComponents/StyleSelectorCard.jsx
+++ client/src/Main/WidgetRandy/Components/SmallerComponents/StyleSelectorCard.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
-const StyleSelectorCard = ({ entry, setStyle, index }) => {
-  return (<option value={index}>Style: {entry.name}</option>)
+const StyleSelectorCard = ({ entry, style, setStyle, index }) => {
+  if (index === style) {
+    return (
+      <SelectedContainer>
+        <StyleDumbNail src={entry.thumbnail_url} onClick={() => setStyle(index)}></StyleDumbNail>
+        <CheckContainer>
+          <FontAwesomeIcon icon={icon({ name: 'circle-check' })} />
+        </CheckContainer>
+      </SelectedContainer>
+    )
+  } else {
+    return (<StyleDumbNail src={entry.thumbnail_url} onClick={() => setStyle(index)}></StyleDumbNail>)
+  }
 }
+
+const StyleDumbNail = styled.img`
+  border-radius: 50%;
+  height: 4rem;
+  width: 4rem;
+`
+const SelectedContainer = styled.div`
+position:relative;
+`
+
+const CheckContainer = styled.div`
+color:green;
+position:absolute;
+top:0;
+right:0
+`
 
 export default StyleSelectorCard
